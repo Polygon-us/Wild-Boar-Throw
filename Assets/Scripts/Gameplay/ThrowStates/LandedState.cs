4d32cf51debb1b45f45f5d17f7fccd85f42b27b4
J. Yepes <jeronimo_y_g@hotmail.com>
2025-03-19 14:41:40 -0500

    make all materials unlit, do some color polish

--- Assets/Scripts/Gameplay/ThrowStates/LandedState.cs
+++ Assets/Scripts/Gameplay/ThrowStates/LandedState.cs
@@ -7,6 +7,7 @@ public class LandedState : StateBase
     [SerializeField] private Transform hideOnLanding;
     [SerializeField] private CinemachineTargetGroup targetGroup;
     [SerializeField] private float landingCamTransitionDuration = 2f;
+    [SerializeField] private Vector3 hideOnLandingPosition = new Vector3(-35f, 0.2f, 70f);
 
     public override void OnEnterState(StateMachine stateMachine)
     {
@@ -15,17 +16,17 @@ public class LandedState : StateBase
         cameraController.ShowLanding();
 
         LeanTween.value(gameObject, 1f, 0f, landingCamTransitionDuration)
-            .setEase(LeanTweenType.easeInOutCubic).setDelay(1.5f)
+            .setEase(LeanTweenType.easeInOutCubic).setDelay(1.5f) // Delay needs to be the same as in the cinemachine custom blends
             .setOnUpdate((value) =>
             {
                 targetGroup.Targets[^1].Weight = value + 1;
                 targetGroup.Targets[0].Weight = 1 - value;
 
-                if (value <= 0.5)
+                /*if (value <= 0.5)
                 {
                     hideOnLanding.localPosition = new Vector3(-35f, 0.2f, 70f);
                     hideOnLanding.eulerAngles = new Vector3(0, 0, 20);
-                }
+                }*/
             });
     }
 
@@ -36,7 +37,7 @@ public class LandedState : StateBase
         targetGroup.Targets[^1].Weight = 1;
         targetGroup.Targets[0].Weight = 0;
 
-        hideOnLanding.localPosition = new Vector3(-21.5f, 0f, 70.5f);
-        hideOnLanding.eulerAngles = new Vector3(0, 0, 0);
+        // hideOnLanding.localPosition = new Vector3(-13.1f, 0f, 70.5f);
+        // hideOnLanding.eulerAngles = new Vector3(0, 0, 0);
     }
 }
