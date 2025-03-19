888a0fe250db67ee8c0aff77d345d015a481ff2c
Juaner <juan.ramirez@polygon.us>
2025-03-19 15:51:28 -0500

    Move assets to folders

---
+++ Assets/Scripts/UI/MainMenu/MainMenuController.cs
@@ -0,0 +1,36 @@
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UI.MainMenu
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button exitButton;
+
+        private void Awake()
+        {
+            startButton.onClick.AddListener(StartGame);
+            exitButton.onClick.AddListener(ExitGame);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        private void ExitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
