ee54ddc6e053f9a41926db47f438b74ac47d7d60
Juaner <juan.ramirez@polygon.us>
2025-03-25 08:04:11 -0500

    open and close tutorial with tween

--- Assets/Scripts/Gameplay/Controllers/TutorialController.cs
+++ Assets/Scripts/Gameplay/Controllers/TutorialController.cs
@@ -7,29 +7,69 @@ namespace Gameplay.Controllers
 {
     public class TutorialController : MonoBehaviour
     {
-        [SerializeField] private GameObject tutorialPanel;
+        [SerializeField] private RectTransform tutorialPanel;
+        [SerializeField] private RectTransform banner;
         [SerializeField] private TMP_Text messageText;
         [SerializeField] private Button closeBtn;
+        [SerializeField] private float tweenTime = 0.5f;
+        [SerializeField] private LeanTweenType tweenType = LeanTweenType.easeOutCubic;
 
         private Action OnClose;
+
+        private Image _backgroundImage;
+        private CanvasScaler _canvasScaler;
+
+        private float UpPosition => _canvasScaler.referenceResolution.y;
         
         private void Awake()
         {
-            closeBtn.onClick.AddListener(Close);
-            Close();
+            _backgroundImage = tutorialPanel.GetComponent<Image>();
+            _canvasScaler = tutorialPanel.GetComponentInParent<CanvasScaler>();
+            closeBtn.onClick.AddListener(() => Close());
+            Internal_Close();
         }
 
         public void Open(string text, Action onClose)
         {
-            tutorialPanel.SetActive(true);
+            tutorialPanel.gameObject.SetActive(true);
             messageText.text = text;
             OnClose = onClose;
+
+            banner.anchoredPosition = new Vector2(banner.anchoredPosition.x, UpPosition);
+
+            LeanTween.value(0f, 0.5f, tweenTime)
+                .setEase(tweenType)
+                .setOnUpdate(SetColor);
+            LeanTween.moveY(banner, 0, tweenTime)
+                .setEase(tweenType);
+        }
+
+        private void SetColor(float value)
+        {
+            Color c = _backgroundImage.color;
+            c.a = value;
+            _backgroundImage.color = c;
         }
 
         private void Close()
         {
-            tutorialPanel.SetActive(false);
-            OnClose?.Invoke();
+            LeanTween.value(0.5f, 0.0f, tweenTime)
+                .setEase(tweenType)
+                .setOnUpdate(SetColor);
+
+            LeanTween.moveY(banner, UpPosition, tweenTime)
+                .setEase(tweenType)
+                .setOnComplete(() =>
+                {
+                    tutorialPanel.gameObject.SetActive(false); 
+                    OnClose?.Invoke();
+                });
+        }
+
+        private void Internal_Close()
+        {
+            tutorialPanel.gameObject.SetActive(false);
+            OnClose = null;
         }
     }
 }
